021e798e816eb3f983cd1d64ee3cee3d96cc6de3
Jaskaran Singh <130739668+Jaskaran-singh1@users.noreply.github.com>
2024-03-08 23:18:07 -0700

    Create ToDoList.js

---
+++ components/ToDoList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { ScrollView, View, Text, Pressable, StyleSheet } from "react-native";
+
+const ToDoList = ({ tasks }) => {
+  return (
+    <ScrollView>
+      {tasks.map((item) => {
+        return (
+          <Pressable>
+            <View style={[styles.task]}>
+              <Text style={styles.taskText}>{item}</Text>
+            </View>
+          </Pressable>
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+  },
+  completed: {
+    backgroundColor: "#e0e0e0",
+  },
+  taskText: {
+    fontSize: 16,
+  },
+});
+
+export default ToDoList;
